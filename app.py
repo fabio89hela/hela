2342a987b163f539ba370941a444d8b195cb4396
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2022-06-20 20:31:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import os.path
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.errors import HttpError
+from google.oauth2 import service_account
 
 from google.oauth2 import service_account
 
