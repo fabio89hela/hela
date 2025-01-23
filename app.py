ed0c598d2cae767969e0127940a8f4c9d6b21c75
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-01-23 18:07:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,20 @@ import firebase_admin
 from firebase_admin import credentials, db
 import time
 
-# Configurazione Firebase
+# Inizializza Firebase utilizzando i secrets di Streamlit
 if not firebase_admin._apps:
-    cred = credentials.Certificate("path/to/your-firebase-credentials.json")
+    cred = credentials.Certificate({
+        "type": st.secrets["type"],
+        "project_id": st.secrets["project_id"],
+        "private_key_id": st.secrets["private_key_id"],
+        "private_key": st.secrets["private_key"].replace("\\n", "\n"),
+        "client_email": st.secrets["client_email"],
+        "client_id": st.secrets["client_id"],
+        "auth_uri": st.secrets["auth_uri"],
+        "token_uri": st.secrets["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["client_x509_cert_url"]
+    })
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://<your-database-name>.firebaseio.com/'
     })
