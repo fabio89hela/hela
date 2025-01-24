03fe04b5e242831a55e24839f5c85a09b893cea7
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-01-24 09:25:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ a=0
 
 st.title("AIOM - TORINO")
 if a==0:
-    if st.link_button("Riscaldamento", "https://game.helaglobe.com/main//game?game=623-8bb7fe84d0c4823efd469dfbf2dccc13"):
-        a=1
-else:
+    st.link_button("Riscaldamento", "https://game.helaglobe.com/main//game?game=623-8bb7fe84d0c4823efd469dfbf2dccc13")
+elif a==1:
     st.link_button("Corsa", "https://game.helaglobe.com/main//game?game=624-3feefa2ef9a347a0423cc694246871c7")
 time.sleep(1)
 st.rerun()
