f6f83e632617a7ea2ef5e1a23f1de61cb2dc6a2a
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-01-24 09:18:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
+a=0
 
 st.title("AIOM - TORINO")
-if st.link_button("Riscaldamento", "https://game.helaglobe.com/main//game?game=623-8bb7fe84d0c4823efd469dfbf2dccc13"):
+if a==0:
+    if st.link_button("Riscaldamento", "https://game.helaglobe.com/main//game?game=623-8bb7fe84d0c4823efd469dfbf2dccc13"):
+        a=1
+else:
     st.link_button("Corsa", "https://game.helaglobe.com/main//game?game=624-3feefa2ef9a347a0423cc694246871c7")
 st.rerun()
     
