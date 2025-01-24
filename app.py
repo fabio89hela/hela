677df3aa40477f5d4f5df1a6dd9d0239a7dc0b53
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-01-24 15:04:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -41,12 +41,9 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-a=0
+a=4
 
 st.image("logo.jpg")
-
-st.title("AIOM - TORINO")
-
 
 if a==0: #riscaldamento
     st.link_button("Riscaldamento", "https://game.helaglobe.com/main//game?game=623-8bb7fe84d0c4823efd469dfbf2dccc13")
