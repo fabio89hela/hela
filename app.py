798507d08b31e9069de689b88ab31e4f8173b813
fabio89hela <107808615+fabio89hela@users.noreply.github.com>
2025-01-24 15:22:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ st.markdown("""
 
 a=4
 
-st.image("logo.jpg",width=200)
+st.image("logo.jpg",width=1200)
 
 if a==0: #riscaldamento
     st.link_button("Riscaldamento", "https://game.helaglobe.com/main//game?game=623-8bb7fe84d0c4823efd469dfbf2dccc13")
